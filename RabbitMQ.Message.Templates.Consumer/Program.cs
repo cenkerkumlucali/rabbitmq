b220a0cc846af42d25a924343c43f0612d52b729
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2023-07-13 20:52:31 +0300

    commit

---
+++ RabbitMQ.Message.Templates.Consumer/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Bağlantı oluşturma.
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("URI");
+
+//Bağlantıyı aktifleştirme ve kanal açma.
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P (Point-to-Poing) Tasarımı
+
+// string queueName = "example-p2p-queue";
+// channel.QueueDeclare(
+//     queue: queueName,
+//     durable: false,
+//     exclusive: false,
+//     autoDelete: false
+// );
+// EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+// channel.BasicConsume(queue: queueName, autoAck: false,consumer:consumer);
+//
+// consumer.Received += (sender, eventArgs) =>
+// {
+//     string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+//     Console.WriteLine(message);
+// };
+
+#endregion
+
+#region Publish/Subsribe (Pub/Sub) Tasarımı
+
+// string exchangeName = "example-pub-sub-exchange";
+// channel.ExchangeDeclare(
+//     exchange: exchangeName,
+//     type: ExchangeType.Fanout);
+//
+// string queueName = channel.QueueDeclare().QueueName;
+//
+// channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: string.Empty);
+//
+// EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+//
+// channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+//
+// channel.BasicQos(
+//     prefetchSize: 0,
+//     prefetchCount: 1,
+//     global: false);
+//
+// consumer.Received += (sender, eventArgs) =>
+// {
+//     string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+//     Console.WriteLine(message);
+// };
+
+#endregion
+
+#region Work Queue(İş Kuyruğu) Tasarımı
+
+#endregion
+
+#region Request/Response Tasarımı
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(
+    queue: requestQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+    Console.WriteLine($"{message}");
+    byte[] responseMessage = Encoding.UTF8.GetBytes($"İşlem tamamlandı :{message}");
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.CorrelationId = eventArgs.BasicProperties.CorrelationId;
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: eventArgs.BasicProperties.ReplyTo,
+        body: responseMessage,
+        basicProperties: properties
+    );
+};
+
+#endregion
+
+Console.Read();
---
+++ RabbitMQ.Message.Templates.Publisher/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Bağlantı oluşturma.
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("URI");
+
+//Bağlantıyı aktifleştirme ve kanal açma.
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P (Point-to-Poing) Tasarımı
+
+//
+// string queueName = "example-p2p-queue";
+// channel.QueueDeclare(
+//     queue: queueName,
+//     durable: false,
+//     exclusive: false,
+//     autoDelete: false
+// );
+// byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+// channel.BasicPublish(exchange: "", routingKey: queueName, body: message);
+
+#endregion
+
+#region Publish/Subsribe (Pub/Sub) Tasarımı
+
+// string exchangeName = "example-pub-sub-exchange";
+// channel.ExchangeDeclare(
+//     exchange: exchangeName,
+//     type: ExchangeType.Fanout);
+// for (int i = 0; i < 100; i++)
+// {
+//     byte[] message = Encoding.UTF8.GetBytes("Merhaba "+i);
+//     channel.BasicPublish(exchange: exchangeName, routingKey: string.Empty, body: message);
+// }
+
+#endregion
+
+#region Work Queue(İş Kuyruğu) Tasarımı
+
+#endregion
+
+#region Request/Response Tasarımı
+
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+string replyQueueName = channel.QueueDeclare().QueueName;
+string correlationId = Guid.NewGuid().ToString();
+
+#region Request Mesajını Oluşturma ve Gönderme
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+properties.ReplyTo = replyQueueName;
+for (int i = 0; i < 100; i++)
+{
+
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: requestQueueName,
+        body: message,
+        basicProperties: properties
+    );
+}
+
+#endregion
+
+#region Response Kuyruğu Dinleme
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(
+    queue: replyQueueName,
+    autoAck: true,
+    consumer: consumer);
+consumer.Received += async (sender, eventArgs) =>
+{
+    if (eventArgs.BasicProperties.CorrelationId == correlationId)
+    {
+        string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+        Console.WriteLine($"Response :{message}");
+    }
+};
+
+#endregion
+
+#endregion
+
+
+Console.Read();
